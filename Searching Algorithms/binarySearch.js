3526ad488b19c1ca0d3c6d72fda77fcd01071ca9
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-08-12 15:11:32 +0530

    binary search

---
+++ Searching Algorithms/binarySearch.js
@@ -0,0 +1,11 @@
+function binarySearch(arr,val) {
+    let start = 0
+    let end = arr.length - 1
+    while(start<=end){
+        let mid = Math.floor((end + start)/2)
+        if(arr[mid]===val) return mid;
+        if(val>arr[mid]) start=mid+1;
+        if(val<arr[mid]) end=mid-1;
+    }
+    return -1
+}
--- Searching Algorithms/linearSearch.js
+++ Searching Algorithms/linearSearch.js
@@ -1,8 +1,8 @@
 //Simple Linear Search
-//Time Complexity O(n)
 function linearSearch(arr,val) {
     for (let i = 0; i < arr.length; i++) {
         if(arr[i]===val) return i; 
     }
     return -1;
 }
+
