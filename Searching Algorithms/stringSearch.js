5c6e5194a0149eefdb3d378b942c73ecaab92135
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-08-14 21:35:22 +0530

    stringSearch Logic Change

--- Searching Algorithms/stringSearch.js
+++ Searching Algorithms/stringSearch.js
@@ -1,16 +1,20 @@
 //My Naive Approach
 //Time Complexity O(n^2)
 function stringSearchNaive(main,subs) {
+    let matches = 0
     for (let i = 0; i < main.length; i++) {
         if(main[i]===subs[0]){
             let count=1
             for (let j = 1; j < subs.length; j++) {
                 if(subs[j]===main[i+j]) count++;
             }
-            if(count===subs.length) return "Contains"
+            if(count===subs.length){
+                count=1;
+                matches++;
+            }
         }
     }
-    return "Doesn't Contain"
+    return matches ? `${matches} Matches` : "Doesn't Contain"
 }
 
 //More Efficient Solution
