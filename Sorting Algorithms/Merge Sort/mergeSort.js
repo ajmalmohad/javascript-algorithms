b4a99d9fd0f9b5095e463f757cc3ccd071fa6f2d
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-09-13 10:57:56 +0530

    merge

--- Sorting Algorithms/Merge Sort/mergeSort.js
+++ Sorting Algorithms/Merge Sort/mergeSort.js
@@ -5,3 +5,10 @@ function mergeSort(arr) {
 
 //Export for Test Driven Development
 module.exports = mergeSort
+
+//Combine Two sorted Array to One Sorted Array
+function merge(arr1,arr2){
+    
+}
+
+console.log(merge([1,2,5,6],[-1,3,4,7]));
