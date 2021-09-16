e8a015024110116e8e2c5297a05ae423a93c8413
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-09-16 19:34:07 +0530

    Quick Sort

--- Sorting Algorithms/Quick Sort/quickSort.js
+++ Sorting Algorithms/Quick Sort/quickSort.js
@@ -1,4 +1,27 @@
-function quickSort(arr) {
+function quickSort(arr,start,end) {
+    if(start>=end)return
+    if(!arr.length)return []
+    let index = pivotPosition(arr,start,end)
+    quickSort(arr,start,index-1)
+    quickSort(arr,index+1,end)
+    return arr
+}
+
+function pivotPosition(arr,start,end) {
+    pivotIndex = start
+    pivot = arr[end]
+    for (let i = start; i <= end; i++) {
+        if(arr[i]<pivot){
+            arr = swap(arr,i,pivotIndex)
+            pivotIndex++
+        }
+    }
+    arr = swap(arr,pivotIndex,end)
+    return pivotIndex
+}
+
+function swap(arr,a,b){
+    [arr[a],arr[b]] = [arr[b],arr[a]]
     return arr
 }
 
--- Sorting Algorithms/Quick Sort/quickSort.test.js
+++ Sorting Algorithms/Quick Sort/quickSort.test.js
@@ -1,19 +1,19 @@
 const quickSort = require("./quickSort.js");
 
 test("All Number Sorting", () => {
-    expect(quickSort([2,10,-10,4,3,0,-3,4])).toStrictEqual([-10,-3,0,2,3,4,4,10]);
+    expect(quickSort([2,10,-10,4,3,0,-3,4],0,7)).toStrictEqual([-10,-3,0,2,3,4,4,10]);
 });
 
 test("Negetive Sorting", () => {
-    expect(quickSort([-10,-5,-7,0,-2,-1])).toStrictEqual([-10,-7,-5,-2,-1,0]);
+    expect(quickSort([-10,-5,-7,0,-2,-1],0,5)).toStrictEqual([-10,-7,-5,-2,-1,0]);
 });
 
 test("Positive Sorting", () => {
-    expect(quickSort([10,5,7,0,2,1])).toStrictEqual([0,1,2,5,7,10]);
+    expect(quickSort([10,5,7,0,2,1],0,5)).toStrictEqual([0,1,2,5,7,10]);
 });
 
 test("Float Sorting", () => {
-    expect(quickSort([10.5,-5.4,5.3,-2.1,0.8,2,1.9])).toStrictEqual([-5.4,-2.1,0.8,1.9,2,5.3,10.5]);
+    expect(quickSort([10.5,-5.4,5.3,-2.1,0.8,2,1.9],0,6)).toStrictEqual([-5.4,-2.1,0.8,1.9,2,5.3,10.5]);
 });
 
 test("Empty Array", () => {
