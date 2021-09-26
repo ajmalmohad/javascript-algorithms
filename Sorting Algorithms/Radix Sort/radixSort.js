e7e0a448e597429a436b87be4e6ed805b53d89fd
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-09-26 12:41:31 +0530

    Radix Sort Helpers

--- Sorting Algorithms/Radix Sort/radixSort.js
+++ Sorting Algorithms/Radix Sort/radixSort.js
@@ -1,3 +1,34 @@
 function radixSort(arr) {
     return arr
 }
+
+
+// Helpers
+/**
+ * @function mostDigits -> Number of Digits in Largest Numbers in Array
+ * @function digitCount -> Number of Digits in a Number
+ * @function getDigit -> Digit at given place
+ * @function getDigit2 -> Digit at given place
+ */
+
+function mostDigits(arr) {
+    let most = 0
+    for (let i = 0; i < arr.length; i++) {
+        most=Math.max(most,digitCount(arr[i]))
+    }
+    return most
+}
+
+function digitCount(num) {
+    if(num===0) return 1
+    return Math.floor(Math.log10(Math.abs(num))) + 1
+}
+
+function getDigit(num,i) {
+    return  Math.floor(Math.abs(num)/Math.pow(10,i))%10
+}
+
+function getDigit2(num,i) {
+    num = num.toString().split("").reverse()
+    return num[i] ? parseInt(num[i]) : 0
+}
