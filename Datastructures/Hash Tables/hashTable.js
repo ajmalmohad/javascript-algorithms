95d9cd21b0acab61825f323c39c49ba5ff4f854d
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-12-05 21:49:50 +0530

    Simple Hash Table - Chaining w/ Arrays

--- Datastructures/Hash Tables/hashTable.js
+++ Datastructures/Hash Tables/hashTable.js
@@ -2,29 +2,7 @@ class Node{
     constructor(key,value){
         this.key = key
         this.value = value
-        this.next = null
     }
-}
-
-class SinglyLinkedList{
-    constructor(){
-        this.head = null
-        this.tail = null
-        this.length = 0
-    }
-
-    push(node){
-        if(this.head === null){
-            this.head = node
-            this.tail = this.head
-        }else{
-            this.tail.next = node
-            this.tail = this.tail.next
-        }
-        this.length++
-        return this.head
-    }
-
 }
 
 class HashTable{
@@ -40,29 +18,22 @@ class HashTable{
     }
     set(key,value){
         let node = new Node(key,value)
-        let hashedKey = this._hash(key)
-        if(this.table[hashedKey]==null){
-            let ll = new SinglyLinkedList()
-            this.table[hashedKey] = ll
-            this.table[hashedKey].push(node)
-        }else{
-            this.table[hashedKey].push(node)
-        }
+        let index = this._hash(key)
+        if(!this.table[index]) this.table[index] = []
+        this.table[index].push(node)
     }
     get(key){
-        let hashedKey = this._hash(key)
-        if(this.table[hashedKey]==null){
+        let index = this._hash(key)
+        if(this.table[index]==null){
             return null
         }else{
-            let node =  this.table[hashedKey].head
-            while(node){
-                if(node.key === key){
-                    return node
+            for (let i = 0; i < this.table[index].length; i++) {
+                if(this.table[index][i].key === key){
+                    return this.table[index][i]
                 }
-                node = node.next
             }
-            return null
         }
+        return null
     }
 }
 
@@ -70,4 +41,5 @@ let ht = new HashTable(53);
 ht.set("ajmal",100)
 ht.set("lamja",500)
 ht.set("hello",100)
-console.log(ht.get("ajmal"))
+ht.set("clear","Hi")
+console.log(ht.get("clear"))
