0dfbdc9c3f1f0602d82a251921ce389f362da974
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-12-18 12:58:16 +0530

    U-Graph Remove Edge

--- Datastructures/Graphs/graph.js
+++ Datastructures/Graphs/graph.js
@@ -21,11 +21,27 @@ class Graph{
             console.log('Vertex Doesn\'t Exist')
         }
     }
+
+    removeEdge(vertex1,vertex2){
+        if(this.adjacencyList[vertex1] && this.adjacencyList[vertex2]){
+            if(this.adjacencyList[vertex1].includes(vertex2) && this.adjacencyList[vertex2].includes(vertex1)){
+                this.adjacencyList[vertex1].splice(this.adjacencyList[vertex1].indexOf(vertex2),1)
+                this.adjacencyList[vertex2].splice(this.adjacencyList[vertex2].indexOf(vertex1),1)
+            }else{
+                console.log('Edge Doesn\'t Exist')
+            }
+        }else{
+            console.log('Vertex Doesn\'t Exist')
+        }
+    }
 }
 
 gph = new Graph()
-gph.addVertex("Vertex1")
-gph.addVertex("Vertex2")
-gph.addEdge("Vertex2","Vertex1")
-gph.addEdge("Vertex1","Vertex2")
+gph.addVertex("Tokyo")
+gph.addVertex("Dallas")
+gph.addVertex("Aspen")
+gph.addEdge("Tokyo","Dallas")
+gph.addEdge("Dallas","Aspen")
+gph.addEdge("Aspen","Tokyo")
+gph.removeEdge("Aspen","Tokyo")
 console.log(gph.adjacencyList)
