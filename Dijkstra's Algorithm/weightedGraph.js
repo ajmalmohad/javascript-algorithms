cba809d1c690ae3028bdd7ce88db333e098a3cf1
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2021-12-25 15:48:47 +0530

    Dijkstra's Start

--- Dijkstra's Algorithm/weightedGraph.js
+++ Dijkstra's Algorithm/weightedGraph.js
@@ -1,3 +1,52 @@
+class Node{
+    constructor(value,priority){
+        this.value = value;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue{
+    constructor(){
+        this.values = []
+    }
+    enqueue(value,priority){
+        let node = new Node(value,priority)
+        this.values.push(node)
+        let current = this.values.length - 1
+        while(current > 0 ){
+            let parent = Math.floor((current-1)/2)
+            if(this.values[current].priority>=this.values[parent].priority) break 
+            [this.values[parent], this.values[current]] = [this.values[current], this.values[parent]]
+            current = parent
+        }
+    }
+    dequeue(){
+        let removed = this.values.shift()
+        if(!this.values.length) return removed
+        this.values.unshift(this.values.pop())
+        let current = 0;
+        let smallestChild;
+        while(true){
+            if((current*2)+1>=this.values.length){
+                break
+            }else if((current*2)+2==this.values.length){
+                smallestChild = (current*2)+1
+            }else{
+                smallestChild = (this.values[(current*2)+1].priority<this.values[(current*2)+2].priority) ? (current*2)+1 : (current*2)+2
+            }
+    
+            if(this.values[smallestChild].priority < this.values[current].priority){
+                [this.values[smallestChild],this.values[current]] = [this.values[current],this.values[smallestChild]]
+            }else{
+                break
+            }
+            current = smallestChild
+        }
+        return removed
+    }
+}
+
+
 class WeightedGraph{
     constructor(){
         this.adjacencyList = {}
@@ -24,6 +73,27 @@ class WeightedGraph{
         }
     }
 
+    dijkstra(start,finish){
+        const nodes = new PriorityQueue()
+        const distances = {}
+        const previous = {}
+
+        //Initial State
+        for(let vertex in this.adjacencyList){
+            if(vertex !== start){
+               distances[vertex] = Infinity
+               nodes.enqueue(vertex, Infinity)
+            } 
+            else{
+                distances[vertex] = 0
+                nodes.enqueue(vertex, 0)
+            }
+            previous[vertex] = null
+        }
+
+        
+    }
+
 }
 
 let pgh = new WeightedGraph();
@@ -41,4 +111,5 @@ pgh.addEdge("C","D",2)
 pgh.addEdge("C","F",4)
 pgh.addEdge("D","F",1)
 pgh.addEdge("E","F",1)
-console.log(pgh.adjacencyList)
+// console.log(pgh.adjacencyList)
+pgh.dijkstra('A','B')
